a7c58cd588f2d1e302f9294c4b2702acb3930b0d
Alberto Sancho <designalbertosancho@gmail.com>
2024-11-06 01:07:26 +0100

    fetch

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import './style.css'
+
+const searchPhotos = (keyword, color) => {
+  document.querySelector("#container").innerHTML = "";
+  fetch(
+    `https://api.unsplash.com/search/photos?client_id=Y8oAt_roiMdXG5on9dAZrMCAzmctmkKKgMHtNhPiuHU&query=${keyword}`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      printPhotos(res.results);
+    });
+};
+
+const printPhotos = (photos) => {
+  const container = document.querySelector("#container");
+
+  for (const photo of photos) {
+    container.innerHTML += `
+    <img src="${photo.urls.regular}" alt="${photo.alt_description}" loading="lazy" />
+    `;
+  }
+};
+
+document.querySelector("#searchBtn").addEventListener("click", () => {
+  searchPhotos(document.querySelector("#searchInput").value);
+});
